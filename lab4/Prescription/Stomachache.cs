fb3ba87a3af25e466c89aec3e316d3ed04891a1d
SunnyIdahosa18191 <SunnyIdahosa@protonmail.com>
2024-04-02 13:52:47 +0300

    add new routes

---
+++ lab4/Prescription/Stomachache.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4.Prescription
+{
+    class Stomachache : PrescriptionBase
+    {
+        public Stomachache()
+        {
+            description = "Моторикс, метоклопрамід, домрид";
+            validity = DateTime.Now.AddDays(7);
+        }
+        public override DateTime GetValidity()
+        {
+            return validity;
+        }
+    }
+}
