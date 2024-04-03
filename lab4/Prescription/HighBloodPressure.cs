f805f3f90a58c95e483c36908584e3064d752684
SunnyIdahosa18191 <SunnyIdahosa@protonmail.com>
2024-04-03 13:52:47 +0300

    Fix bug in date formatting

---
+++ lab4/Prescription/HighBloodPressure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4.Prescription
+{
+    internal class HighBloodPressure : PrescriptionBase
+    {
+        public HighBloodPressure()
+        {
+            description = "Каптопрес, фуросемід, конкор, трипліксам";
+            validity = DateTime.Now.AddMonths(1);
+        }
+
+        public override DateTime GetValidity()
+        {
+            return validity;
+        }
+    }
+}
