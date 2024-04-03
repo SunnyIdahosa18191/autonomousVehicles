5d36e20953d7690b2f6dd5335bd48fba957113b4
SunnyIdahosa18191 <SunnyIdahosa@protonmail.com>
2024-04-03 13:52:47 +0300

    improve database queries

---
+++ lab4/Prescription/Headache.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4.Prescription
+{
+    class Headache : PrescriptionBase
+    {
+        public Headache()
+        {
+            description = "Парацетамол, анальгін, ібупрофен";
+            validity = DateTime.Now.AddDays(14);
+        }
+
+        public override DateTime GetValidity()
+        {
+            return validity;
+        }
+    }
+}
