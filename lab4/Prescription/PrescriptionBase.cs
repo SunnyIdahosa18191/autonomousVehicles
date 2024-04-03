7ba0aefbe46c25165fef6295053bafd06ba2fef2
SunnyIdahosa18191 <SunnyIdahosa@protonmail.com>
2024-04-03 13:52:47 +0300

    fixed css issues

---
+++ lab4/Prescription/PrescriptionBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4.Prescription
+{
+    public abstract class PrescriptionBase
+    {
+        protected string description;
+        protected DateTime validity;
+        public string GetDescription()
+        {
+            return description;
+        }
+        public abstract DateTime GetValidity();
+    }
+}
