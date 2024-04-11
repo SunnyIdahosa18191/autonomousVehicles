36ec8a641b19184ebf2c73c3e8f7ac8cad1791df
SunnyIdahosa18191 <SunnyIdahosa@protonmail.com>
2024-04-11 13:52:47 +0300

    add new endpoints

---
+++ lab4/Decorators/SeriousIllness.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using lab4.Prescription;
+
+namespace lab4.Decorators
+{
+    class SeriousIllness : AddDaysBase
+    {
+        private PrescriptionBase _prescription;
+        public SeriousIllness(PrescriptionBase prescription)
+        {
+            _prescription = prescription;
+            description = _prescription.GetDescription();
+            validity = _prescription.GetValidity().AddDays(14);
+        }
+        public override DateTime GetValidity()
+        {
+            return validity;
+        }
+    }
+}
