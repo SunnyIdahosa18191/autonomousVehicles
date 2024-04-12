69de5376d7dc2636657e244009fdc2bc7b5d75c3
SunnyIdahosa18191 <SunnyIdahosa@protonmail.com>
2024-04-12 13:52:47 +0300

    Update user model with new fields

---
+++ lab4/Decorators/ModerateIllness.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using lab4.Prescription;
+
+namespace lab4.Decorators
+{
+    class ModerateIllness : AddDaysBase
+    {
+        private PrescriptionBase _prescription;
+        public ModerateIllness(PrescriptionBase prescription)
+        {
+            _prescription = prescription;
+            description = _prescription.GetDescription();
+            validity = _prescription.GetValidity().AddDays(7);
+        }
+
+        public override DateTime GetValidity()
+        {
+                return validity;
+        }
+    }
+}
