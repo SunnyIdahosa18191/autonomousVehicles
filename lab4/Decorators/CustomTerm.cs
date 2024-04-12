14844c87ba2bb4f0ba5be8eeaa0adde6b07293af
SunnyIdahosa18191 <SunnyIdahosa@protonmail.com>
2024-04-12 13:52:47 +0300

    improve performance

---
+++ lab4/Decorators/CustomTerm.cs
@@ -0,0 +1,23 @@
+﻿using lab4.Prescription;
+using System;
+
+namespace lab4.Decorators
+{
+    internal class CustomTerm : AddDaysBase
+    {
+        int n;
+        private PrescriptionBase _prescription;
+        public CustomTerm(PrescriptionBase prescription, int n=0)
+        {
+            this.n = n;
+            _prescription = prescription;
+            description = _prescription.GetDescription();
+            validity = _prescription.GetValidity().AddDays(n);
+        }
+
+        public override DateTime GetValidity()
+        {
+            return validity;
+        }
+    }
+}
