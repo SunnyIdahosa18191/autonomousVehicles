66ae0d67a9081cf566e8bffe7f20106b397c374a
SunnyIdahosa18191 <SunnyIdahosa@protonmail.com>
2024-04-13 13:52:47 +0300

    Fix typo in docs

---
+++ lab4/Decorators/AddDaysBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4.Decorators
+{
+    abstract class AddDaysBase : Prescription.PrescriptionBase
+    {
+    }
+}
